66e2601dc5b6d3e53bcce7ea7cd40888df399a30
manucleal <fmanuleal@gmail.com>
2021-03-19 23:59:11 -0300

    Primer commit de prueba

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            SqlConnection myConnection = new SqlConnection();
+            
+            //Hardcodeado en el controller
+            //myConnection.ConnectionString = @"SERVER=(localDb)\MsSqlLocalDb;
+            //                                DATABASE=database_p3; 
+            //                                INTEGRATED SECURITY=TRUE";
+            
+            //Desde web config
+            //myConnection.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+
+            //ViewBag.Message = "Database Connected! ";
+            //myConnection.Open();
+            //consultas
+            //myConnection.Close();
+            //Dominio.Helpers.Validacion;
+
+
+            if (AltaCategoria())
+            {
+                ViewBag.Message = "Se dio de alta";
+            }
+            else
+            {
+                ViewBag.Message = "No se dio de alta";
+            }
+            return View();
+        }
+
+        private bool AltaCategoria()
+        {
+            SqlConnection myConnection = new SqlConnection();
+            //Desde web config
+            myConnection.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = myConnection;
+            cmd.CommandText = "INSERT INTO Categoria VALUES ('Nueva Categoria')";
+            myConnection.Open();
+
+            int filasAfectadas = cmd.ExecuteNonQuery();
+            myConnection.Close();
+            if (filasAfectadas > 0){
+                return true;
+            }
+            return false;
+        }
+    }
+}
