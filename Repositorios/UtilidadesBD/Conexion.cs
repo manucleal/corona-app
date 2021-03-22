c6e8a745fe75f24deee176c42cbb29d416e048c7
manucleal <fmanuleal@gmail.com>
2021-03-22 09:05:47 -0300

    agregando capa de acceso a datos

---
+++ Repositorios/UtilidadesBD/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorios.UtilidadesBD
+{
+    public class Conexion
+    {
+        public string stringConecction { get; set; }
+        public SqlConnection crearConexion()
+        {
+            return new SqlConnection(stringConecction);
+        }
+
+        public Conexion()
+        {
+            stringConecction = System.Configuration.ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+        }
+    }
+}
