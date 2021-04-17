4a1b06170ebc1f299893aa68e1e49232d649255c
fausto137 <faustoperillo@gmail.com>
2021-04-17 17:28:37 -0300

    creando clase laboratorio

--- Dominio/EntidadesNegocio/Laboratorio.cs
+++ Dominio/EntidadesNegocio/Laboratorio.cs
@@ -8,5 +8,19 @@ namespace Dominio.EntidadesNegocio
 {
     class Laboratorio
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string PaisOrigen { get; set; }
+        public bool Experiencia { get; set; }
+
+
+        public bool Validar()
+        {
+            return this.Id > 0 && this.Nombre != "" && this.PaisOrigen != "";
+        }
+
+
+
+
     }
 }
